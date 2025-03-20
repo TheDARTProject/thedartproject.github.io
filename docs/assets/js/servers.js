29487fc8377a3a60ba2580e02b807d17a86f1dd8
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-03-20 09:44:54 +0200

    updated info page for personal server

--- docs/assets/js/servers.js
+++ docs/assets/js/servers.js
@@ -3,12 +3,14 @@
 const serverNames = {
     PROJECT_CW: "Project CW",
     BLITZHUB_EU: "BlitzHUB EU",
+    SINEWAVE_DEVELOPMENT: "SINEWAVE Development",
     ANONYMOUS_SERVER_1: "1 Anonymous Server"
 };
 
 const serverInvites = {
     PROJECT_CW: "https://discord.gg/projectcw",
     BLITZHUB_EU: "https://discord.gg/WKz7RJvtUK",
+    SINEWAVE_DEVELOPMENT: "https://discord.gg/2nHHHBWNDw",
     // ANONYMOUS_SERVER_1 does not need an invite link
 };
 
