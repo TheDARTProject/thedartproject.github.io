2643fd0c971e103c0b45d4145d57e31735a3a947
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-03-20 20:33:04 +0200

    renamed info page placeholder image

--- docs/assets/js/news.js
+++ docs/assets/js/news.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const articleCards = document.querySelectorAll(".article-card");
     const loadingSpinner = document.getElementById("loading-spinner");
     const newsContainer = document.getElementById("news-container");
-    const placeholderImage = "https://raw.githubusercontent.com/ThatSINEWAVE/CDA-Project/refs/heads/main/docs/images/site-images/Placeholder.png";
+    const placeholderImage = "https://raw.githubusercontent.com/ThatSINEWAVE/CDA-Project/refs/heads/main/docs/images/site-images/NOT-LOADED.png";
     const rssFeedUrl = "https://www.digitalvocano.com/cybersecurity/rss/latest-posts";
     const noNewsMessage = document.getElementById("no-news-message");
 
