41cd34bdad709230e18af25957b46df355f68b81
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-04-05 07:10:01 +0300

    Update servers.js

--- docs/assets/js/servers.js
+++ docs/assets/js/servers.js
@@ -8,6 +8,9 @@ const serverNames = {
     ANONYMOUS_SERVER_1: "Anonymous Server #1",
     ANONYMOUS_SERVER_2: "Anonymous Server #2",
     ANONYMOUS_SERVER_3: "Anonymous Server #3",
+    ANONYMOUS_SERVER_4: "Anonymous Server #4",
+    PROJECT_OWL: "Project Owl",
+    CRYPTOCURRENCY: "r/CryptoCurrency",
     BLITZPOST: "Blitz Post"
 };
 
@@ -17,9 +20,12 @@ const serverInvites = {
     BLITZHUB_EU: "https://discord.gg/WKz7RJvtUK",
     SINEWAVE_DEVELOPMENT: "https://discord.gg/2nHHHBWNDw",
     BLITZPOST: "https://discord.com/invite/q82WM4E",
+    PROJECT_OWL: "https://discord.com/invite/projectowl",
+    CRYPTOCURRENCY: "https://discord.com/invite/cryptocurrencyofficial",
     ANONYMOUS_SERVER_1: "",
     ANONYMOUS_SERVER_2: "",
-    ANONYMOUS_SERVER_3: ""
+    ANONYMOUS_SERVER_3: "",
+    ANONYMOUS_SERVER_4: ""
 };
 
 export default {
