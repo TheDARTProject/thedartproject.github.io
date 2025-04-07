4f6bbde71a0a6fd02286d763840e9120be4672dc
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-04-07 09:41:51 +0300

    updated invite links

--- docs/assets/js/servers.js
+++ docs/assets/js/servers.js
@@ -8,7 +8,6 @@ const serverNames = {
     ANONYMOUS_SERVER_1: "Anonymous Server #1",
     ANONYMOUS_SERVER_2: "Anonymous Server #2",
     ANONYMOUS_SERVER_3: "Anonymous Server #3",
-    ANONYMOUS_SERVER_4: "Anonymous Server #4",
     PROJECT_OWL: "Project Owl",
     CRYPTOCURRENCY: "r/CryptoCurrency",
     BLITZPOST: "Blitz Post"
@@ -16,16 +15,15 @@ const serverNames = {
 
 const serverInvites = {
     DIRECT_MESSAGES: "",
-    PROJECT_CW: "https://discord.gg/projectcw",
-    BLITZHUB_EU: "https://discord.gg/WKz7RJvtUK",
-    SINEWAVE_DEVELOPMENT: "https://discord.gg/2nHHHBWNDw",
+    PROJECT_CW: "https://discord.com/invite/projectcw",
+    BLITZHUB_EU: "https://discord.com/invite/WKz7RJvtUK",
+    SINEWAVE_DEVELOPMENT: "https://discord.com/invite/2nHHHBWNDw",
     BLITZPOST: "https://discord.com/invite/q82WM4E",
     PROJECT_OWL: "https://discord.com/invite/projectowl",
     CRYPTOCURRENCY: "https://discord.com/invite/cryptocurrencyofficial",
     ANONYMOUS_SERVER_1: "",
     ANONYMOUS_SERVER_2: "",
-    ANONYMOUS_SERVER_3: "",
-    ANONYMOUS_SERVER_4: ""
+    ANONYMOUS_SERVER_3: ""
 };
 
 export default {
